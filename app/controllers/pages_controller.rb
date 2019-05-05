ad295019667c38d6ce23565779dc7bdf094612d7
Luca de Montis <luca.demo@hotmail.it>
2019-05-05 20:11:15 +0200

    services page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def services
+  end
+
+  def support
+  end
+
+  def about_us
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'services', to: 'pages#services', as: :services
+  get 'support', to: 'pages#support', as: :support
+  get 'about-us', to: 'pages#about_us', as: :about_us
 end
